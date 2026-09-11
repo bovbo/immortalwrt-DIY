81ae6d7604138ec878de9835a3ec574e324ba86c
bovbo <96370622+bovbo@users.noreply.github.com>
2026-09-11 22:46:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,9 +26,8 @@ chmod +x files/etc/uci-defaults/* 2>/dev/null
 sed -i 's/CONFIG_BPF_TOOLCHAIN_HOST=y/# CONFIG_BPF_TOOLCHAIN_HOST is not set/' .config
 sed -i 's/CONFIG_USE_LLVM_HOST=y/# CONFIG_USE_LLVM_HOST is not set/' .config
 
-# 开启 Build 方式
-echo "CONFIG_BPF_TOOLCHAIN_BUILD=y" >> .config
-echo "CONFIG_USE_LLVM_BUILD=y" >> .config
+# 开启 Build 方式（正确的选项名，末尾要带 _LLVM）
+echo "CONFIG_BPF_TOOLCHAIN_BUILD_LLVM=y" >> .config
 
 # 可选：强制重新生成配置（保险）
 ./scripts/feeds install -a
